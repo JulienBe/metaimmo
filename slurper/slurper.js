51f3345cb74811c172490389ba8d324de8adbb08
JulienBe <bertozzijulien@gmail.com>
2019-11-26 21:21:42 +0100

    Get data from all pages

--- slurper/slurper.js
+++ slurper/slurper.js
@@ -1,25 +1,32 @@
-const leboncoin = require('leboncoin-api');
-const fs = require('fs');
+const leboncoin = require('leboncoin-api')
+const fs = require('fs')
 
-var search = new leboncoin.Search()
-    .setPage(1)
-    .setCategory("ventes_immobilieres")
-    .addSearchExtra("price", {min: 150000, max: 350000}) // will add a range of price
-    .setSort({
-        "sort_by": "date",
-        "sort_order": "desc"
+executeSearch(getSearch(1))
+
+function getSearch(page) {    
+    return new leboncoin.Search()
+        .setPage(page)
+        .setCategory("ventes_immobilieres")
+        .addSearchExtra("price", { min: 150000, max: 350000 }) // will add a range of price
+        .setSort({
+            "sort_by": "date",
+            "sort_order": "desc"
+        })
+}
+
+function executeSearch(search) {
+    search.run().then(function (data) {        
+        data.results.forEach(element => {
+            fs.writeFile("./to_process/" + element.id, JSON.stringify(element), function (err) {
+                if (err) {
+                    console.log("FAILED TO SAVE " + element.link);
+                }
+                console.log(element.id + " was saved");
+            })
+        })
+        if (data.nbResult != 0)
+            executeSearch(getSearch(data.page + 1))
+    }, function (err) {
+        console.error(err);
     })
-
-search.run().then(function (data) {
-    data.results.forEach(element => {
-        console.log(element)
-        fs.writeFile("./to_process/" + element.id, JSON.stringify(element), function(err) {
-            if(err) {
-                return console.log("FAILED TO SAVE " + element.link);
-            }        
-            console.log(element.id + " was saved");
-        }); 
-    });
-}, function (err) {
-    console.error(err);
-});    
+}
